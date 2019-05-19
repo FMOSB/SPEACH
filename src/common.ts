40fea3a17f0854c03e419323b27b84e616ba4cfb
FMOSB <49654958+FMOSB@users.noreply.github.com>
2019-05-19 12:38:26 +0100

    Add files via upload

--- src/common.ts
+++ src/common.ts
@@ -1,18 +1,18 @@
 import * as MRESDK from '@microsoft/mixed-reality-extension-sdk'
 
 export class User {
-    public isFarting: boolean
-    public fartSoundActor: MRESDK.Actor
-    public fartCloudActor: MRESDK.Actor
+    public isSpeaking: boolean
+    public speakSoundActor: MRESDK.Actor
+    public speakCloudActor: MRESDK.Actor
 
     public isBlackedOut: boolean
     public blackoutOutwardFacingSphereActor: MRESDK.Actor
     public blackoutInwardFacingSphereActor: MRESDK.Actor
 
     constructor(public id: string, public name: string) {
-        this.isFarting = false
-        this.fartSoundActor = null
-        this.fartCloudActor = null
+        this.isSpeaking = false
+        this.speakSoundActor = null
+        this.speakCloudActor = null
 
         this.isBlackedOut = false
         this.blackoutOutwardFacingSphereActor = null
